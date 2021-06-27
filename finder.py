f3c96e75a5ac77d5e635d0865a8f3775133fe670
amoh-godwin <amohgyebigodwin@gmail.com>
2021-06-27 13:41:24 +0000

    add to find method

--- finder.py
+++ finder.py
@@ -19,25 +19,22 @@ class Finder(QObject):
         f_thread.start()
 
     def _find(self, needle: str):
-        pass
 
-    needle = '55'
-    timer = 0.0
+        timer = 0.0
 
-    item = {}
-    results = 0
+        item = {}
+        results = 0
 
-    def _some_stuff(self):
-        with open('C:\\OxyTech\\Finder\\D_index.txt', 'r', encoding='utf-8') as e_file:
+        with open('C:\\OxyTech\\Finder\\C_index.txt', 'r', encoding='utf-8') as e_file:
             for string in e_file:
                 real = re.split('`', string)
-                self.item[real[0]] = real[1][0:-1]
+                item[real[0]] = real[1][0:-1]
 
         timer = time.time()
-        for each in self.item:
-            if re.findall(self.needle, each):
-                self.results += 1
+        for each in item:
+            if re.findall(needle, each):
+                results += 1
                 print(each)
 
-        print(self.results)
+        print(results)
         print(time.time() - timer)
