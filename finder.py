18690e2d7b05ffd0ee4299cf6649fbff4c00f381
amoh-godwin <amohgyebigodwin@gmail.com>
2021-06-27 09:47:59 +0000

    create Finder class

--- finder.py
+++ finder.py
@@ -1,22 +1,32 @@
 import re
 import time
 
-needle = '55'
-timer = 0.0
+from PyQt5.QtCore import QObject
 
-item = {}
-results = 0
 
-with open('C:\\OxyTech\\Finder\\D_index.txt', 'r', encoding='utf-8') as e_file:
-    for string in e_file:
-        real = re.split('`', string)
-        item[real[0]] = real[1][0:-1]
-        
-timer = time.time()
-for each in item:
-    if re.findall(needle, each):
-        results += 1
-        print(each)
-        
-print(results)
-print(time.time() - timer)
+class Finder(QObject):
+
+
+    def __init__(self):
+        pass
+
+    needle = '55'
+    timer = 0.0
+
+    item = {}
+    results = 0
+
+    def _some_stuff(self):
+        with open('C:\\OxyTech\\Finder\\D_index.txt', 'r', encoding='utf-8') as e_file:
+            for string in e_file:
+                real = re.split('`', string)
+                self.item[real[0]] = real[1][0:-1]
+
+        timer = time.time()
+        for each in self.item:
+            if re.findall(self.needle, each):
+                self.results += 1
+                print(each)
+                
+        print(self.results)
+        print(time.time() - timer)
