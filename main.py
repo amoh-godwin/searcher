06c72daabe9bb46e261eaea6cd4a775ca85d4582
amoh-godwin <amohgyebigodwin@gmail.com>
2021-06-27 09:50:35 +0000

    instantiate finder

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
+from finder import Finder
+
 app = QGuiApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
 engine.load('UI/main.qml')
+
+finder = Finder()
+engine.rootObjects()[0].setProperty('finder', finder)
 engine.quit.connect(app.quit)
 
 sys.exit(app.exec())
